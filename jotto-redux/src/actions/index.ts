b641ba7cfa84581f300a3ac523cc4c149ac628f7
atsushi-h <atsushi.1213eng@gmail.com>
2020-05-20 21:29:47 +0900

    Add successReducer and tests

--- jotto-redux/src/actions/index.ts
+++ jotto-redux/src/actions/index.ts
@@ -1,9 +1,13 @@
+export type Action = {
+  type: string;
+};
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
   SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
-export function correctGuess() {
+export function correctGuess(): Action {
   return { type: actionTypes.CORRECT_GUESS };
 }
--- jotto-redux/src/reducers/successReducer.ts
+++ jotto-redux/src/reducers/successReducer.ts
@@ -1,3 +1,12 @@
-export default (state: any, reducer: any) => {
-  return null;
+import { actionTypes, Action } from '../actions';
+
+export type State = boolean;
+
+export default (state: State = false, action: Action): State => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
