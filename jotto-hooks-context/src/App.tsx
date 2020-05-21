a41d3571ae4c391acbde255e438f9773060d2662
atsushi-h <atsushi.1213eng@gmail.com>
2020-05-21 11:10:42 +0900

    Basic App component and tests

--- jotto-hooks-context/src/App.tsx
+++ jotto-hooks-context/src/App.tsx
@@ -5,7 +5,7 @@ import Congrats from './Congrats';
 
 function App() {
   return (
-    <div className="container">
+    <div className="container" data-test="component-app">
       <h1>Jotto</h1>
       <Congrats success={false} />
       <GuessedWords guessedWords={[]} />
