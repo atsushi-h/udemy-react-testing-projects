81be5a4e383985086cadf90a7f1076283be4ea9f
atsushi-h <atsushi.1213eng@gmail.com>
2020-05-23 20:27:17 +0900

    Console warn test

--- jotto-hooks-context/src/helpers/strings.test.ts
+++ jotto-hooks-context/src/helpers/strings.test.ts
@@ -8,16 +8,31 @@ const strings = {
 };
 
 describe('language string testing', () => {
+  const mockWarn = jest.fn();
+  let originalWarn: Console['warn'];
+
+  beforeEach(() => {
+    originalWarn = console.warn;
+    console.warn = mockWarn;
+  });
+
+  afterEach(() => {
+    console.warn = originalWarn;
+  });
+
   test('returns correct submit string for english', () => {
     const string = getStringByLanguage('en', 'submit', strings);
     expect(string).toBe('submit');
+    expect(mockWarn).not.toHaveBeenCalled();
   });
   test('returns the correct submit string for emoji', () => {
     const string = getStringByLanguage('emoji', 'submit', strings);
     expect(string).toBe('🚀');
+    expect(mockWarn).not.toHaveBeenCalled();
   });
   test('returns english submit string when submit key does not exist for language', () => {
     const string = getStringByLanguage('mermish', 'submit', strings);
     expect(string).toBe('submit');
+    expect(mockWarn).toHaveBeenCalled();
   });
 });
--- jotto-hooks-context/src/helpers/strings.ts
+++ jotto-hooks-context/src/helpers/strings.ts
@@ -43,6 +43,7 @@ function getStringByLanguage(
   strings: LanguageStrings = languageStrings
 ) {
   if (!strings[languageCode] || !strings[languageCode][stringKey]) {
+    console.warn(`Could not get string [${stringKey}] for [${languageCode}]`);
     return strings.en[stringKey];
   }
   return strings[languageCode][stringKey];
