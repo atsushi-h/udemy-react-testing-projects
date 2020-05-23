53ed43a27b940db9a47b79622ba9f04f30922690
atsushi-h <atsushi.1213eng@gmail.com>
2020-05-23 10:21:29 +0900

    Test strings

---
+++ jotto-hooks-context/src/helpers/strings.ts
@@ -0,0 +1,54 @@
+type LanguageCode = 'en' | 'emoji' | 'mermish';
+
+type StringKey =
+  | 'congrats'
+  | 'submit'
+  | 'guessPrompt'
+  | 'guessInputPlaceholder'
+  | 'guessedWords'
+  | 'guessColumnHeader'
+  | 'matchingLettersColumnHeader';
+
+type LanguageStrings = {
+  [code in LanguageCode]: {
+    [key in StringKey]?: string;
+  };
+};
+
+const languageStrings: LanguageStrings = {
+  en: {
+    congrats: 'Congratulations! You guessed the word!',
+    submit: 'Submit',
+    guessPrompt: 'Try to guess the secret word!',
+    guessInputPlaceholder: 'enter guess',
+    guessColumnHeader: 'Guessed Words',
+    guessedWords: 'Guesses',
+    matchingLettersColumnHeader: 'Matching Letters',
+  },
+  emoji: {
+    congrats: '🎯🎉',
+    submit: '🚀',
+    guessPrompt: '🤔🤫🔤',
+    guessInputPlaceholder: '⌨️🤔',
+    guessedWords: '🤷‍🔤',
+    guessColumnHeader: '🤷‍',
+    matchingLettersColumnHeader: '✅',
+  },
+  mermish: {},
+};
+
+function getStringByLanguage(
+  languageCode: LanguageCode,
+  stringKey: StringKey,
+  strings: LanguageStrings = languageStrings
+) {
+  if (!strings[languageCode] || !strings[languageCode][stringKey]) {
+    return strings.en[stringKey];
+  }
+  return strings[languageCode][stringKey];
+}
+
+// for future mocking
+export default {
+  getStringByLanguage,
+};
